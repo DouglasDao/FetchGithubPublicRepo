1dd17e7f8e8ac91f9ab71a1fd1e763224cae333c
DouglasDao <douglas110394@gmail.com>
2020-02-20 09:02:03 +0530

    First commit

---
+++ app/src/main/java/com/github/repo/view/ThirdFragment.kt
@@ -0,0 +1,24 @@
+package com.github.repo.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.github.repo.R
+
+class ThirdFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val root = inflater.inflate(R.layout.fragment_third, container, false)
+
+        return root
+    }
+}
